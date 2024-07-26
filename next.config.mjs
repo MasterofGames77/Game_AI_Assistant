28afb4cb48e4f1a7be487020ae2c82f3e2e48d97
MasterofGames77 <mgambardella16@gmail.com>
2024-07-26 15:02:02 -0400

    mongodb implemented in vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// next.config.mjs
+
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Load environment variables from .env.local file
+dotenv.config({ path: path.resolve(process.cwd(), '.env.local') });
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+    TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
+    TWITCH_REDIRECT_URI: process.env.TWITCH_REDIRECT_URI,
+    RAWG_API_KEY: process.env.RAWG_API_KEY,
+    MONGODB_URI: process.env.MONGODB_URI, // Ensure MongoDB URI is included
+  },
+};
 
 export default nextConfig;
--- pages/api/getConversation.ts
+++ pages/api/getConversation.ts
@@ -6,8 +6,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { userId } = req.query;
 
   try {
+    console.log(`Fetching conversations for user: ${userId}`);
     await connectToMongoDB();
     const conversations = await Question.find({ userId }).sort({ timestamp: -1 });
+    console.log(`Conversations fetched: ${conversations.length} items`);
     res.status(200).json(conversations);
   } catch (error: any) {
     console.error("Error fetching conversations:", error.message);
