ee7a0fc67d43d1e7a8be1233338534a667ce9534
MasterofGames77 <mgambardella16@gmail.com>
2024-08-05 18:59:57 -0400

    updated steam login whether in production or local

--- pages/api/steamLogin.ts
+++ pages/api/steamLogin.ts
@@ -1,15 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const steamRedirectUri = process.env.STEAM_REDIRECT_URI ?? '';
-    const steamRealm = process.env.STEAM_REALM ?? '';
+    const isProduction = process.env.NODE_ENV === 'production';
+    const domain = isProduction ? 'game-ai-assistant-jiy3g1iy2-masterofgames77s-projects.vercel.app' : 'http://localhost:3000';
 
-    // Check if the environment variables are defined
-    if (!steamRedirectUri || !steamRealm) {
-        throw new Error('Missing required environment variables: STEAM_REDIRECT_URI or STEAM_REALM');
-    }
-
-    const steamLoginUrl = `https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(steamRedirectUri)}&openid.realm=${encodeURIComponent(steamRealm)}&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
+    const steamLoginUrl = `https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(domain + '/api/steamCallback')}&openid.realm=${encodeURIComponent(domain)}&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
 
     res.redirect(steamLoginUrl);
 }
