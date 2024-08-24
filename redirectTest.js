577aebc4edecc11093ccaa670a248ab4a525dbbb
MasterofGames77 <mgambardella16@gmail.com>
2024-08-24 13:27:46 -0400

    updated test file

--- redirectTest.js
+++ redirectTest.js
@@ -1,4 +1,4 @@
-const redirectUri = "https://game-ai-assistant.vercel.app/api/twitchCallback";
+const redirectUri = "https://video-game-wingman-57d61bef9e61.herokuapp.com/api/twitchCallback";
 const encodedRedirectUri = encodeURIComponent(redirectUri);
 
 console.log(encodedRedirectUri);
