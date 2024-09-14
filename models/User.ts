fd05b7d503b6ae99ddb492b8cbbdf730ecb5b58f
MasterofGames77 <mgambardella16@gmail.com>
2024-09-14 15:54:42 -0400

    updated user model script

--- models/User.ts
+++ models/User.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   conversations: { type: Array, required: true },
+  hasProAccess: { type: Boolean, default: false },
 }, { collection: 'userID' });
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
