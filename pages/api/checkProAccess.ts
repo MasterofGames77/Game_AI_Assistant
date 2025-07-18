521de5cdc3181df04be83f5d5e9d969812d91d84
MasterofGames77 <mgambardella16@gmail.com>
2025-07-18 19:04:27 -0400

    update pro access check script

--- pages/api/checkProAccess.ts
+++ pages/api/checkProAccess.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { checkProAccess } from '../../utils/proAccessUtil';
+import { connectToWingmanDB } from '../../utils/databaseConnections';
+import User from '../../models/User';
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,9 +18,30 @@ export default async function handler(
       return res.status(400).json({ message: 'Username is required' });
     }
 
+    // Check basic Pro access
     const hasProAccess = await checkProAccess(username, userId);
 
-    return res.status(200).json({ hasProAccess });
+    // Get detailed subscription information
+    let subscriptionStatus = null;
+    if (hasProAccess) {
+      await connectToWingmanDB();
+      const user = await User.findOne({ 
+        $or: [
+          { username },
+          { userId },
+          ...(userId ? [{ userId }] : [])
+        ]
+      });
+
+      if (user && user.getSubscriptionStatus) {
+        subscriptionStatus = user.getSubscriptionStatus();
+      }
+    }
+
+    return res.status(200).json({ 
+      hasProAccess,
+      subscriptionStatus
+    });
   } catch (error) {
     console.error('Error checking Pro access:', error);
     return res.status(500).json({ 
