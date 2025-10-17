6262475125a7b2f63bd40a7c2648d8ce7fd4ce9d
MasterofGames77 <mgambardella16@gmail.com>
2025-10-17 18:09:30 -0400

    implement google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./styles/global.css";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
         <Toaster />
